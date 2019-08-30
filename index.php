28c94275a106d2d3d957d1c75204ab86a9a57ff4
Jason <launch.box.design@outlook.com>
2019-08-30 17:28:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
+    $from = $_POST['email']; 
     $to = 'jatkins.design@gmail.com'; 
     $subject = 'Hello';
     $human = $_POST['human'];
@@ -18,4 +18,16 @@
     } else if ($_POST['submit'] && $human != '4') {
 	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
+?>
+
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+	$recipient = "jatkins.design@gmail.com";
+	$subject = "Contact Form";
+	$mailheader = "From: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	echo "Thank You!";
 ?>
